bef7b3551227871c0c7286102ee27f508d5d0de7
test1 <18810955730@163.com>
2019-03-30 00:02:02 +0800

    finish index全left

---
+++ mall/src/pages/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="wrapper">
+		<div class="detail-left">
+			<div class="product-board">
+				<ul>
+					<li v-for="item in products">{{item.name}}</li>
+				</ul>
+			</div>
+			
+		</div>
+		<div class="detail-right">
+			
+		</div>
+		
+	</div>
+	
+</template>
